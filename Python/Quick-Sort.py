ec03a929762771c28856c25e71f64c4a1f4a5944
sTux <esyray15@gmail.com>
2017-07-09 21:22:52 +0530

    Fixed Quick Sort

--- Python/Quick-Sort.py
+++ Python/Quick-Sort.py
@@ -16,7 +16,7 @@ def partition(arr, low, high):
             arr[i], arr[j] = arr[j], arr[i]
 
     arr[i+1], arr[high] = arr[high], arr[i+1]
-
+    print(arr)
     return i+1
 
 
@@ -25,14 +25,13 @@ def sort(arr, low, high):
         # pi is partitioning index, arr[p] is now
         # at right place
         pi = partition(arr, low, high)
-
         sort(arr, low, pi-1)
         sort(arr, pi+1, high)
 
 
 def main():
-    arr = [randint(0, 100) for _ in range(10)]
-    length = 10
+    arr = [5, 8, 1, 3, 7, 9, 2]  # [randint(0, 100) for _ in range(10)]
+    length = 7  # 10
     print(arr)
     sort(arr, 0, length-1)
     print(arr)
