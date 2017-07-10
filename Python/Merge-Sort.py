5c12b085334111712f1d512c8eea91d8e1c2f5d2
sTux <esyray15@gmail.com>
2017-07-10 21:07:25 +0530

    Added example of Merge Sort in Python

--- Python/Merge-Sort.py
+++ Python/Merge-Sort.py
@@ -8,7 +8,40 @@ Repeatedly merge sublists to produce new sorted sublists until there is only 1 s
 
 
 def merge(arr, low, high, mid):
-    pass
+    n1 = mid - low + 1
+    n2 = high - mid
+
+    left = [0]*n1  # Creating a array of size n1
+    right = [0]*n2  # Creating another array of size n2
+
+    for i in range(n1):
+        left[i] = arr[low+i]
+    for j in range(n2):
+        right[j] = arr[mid+1+j]
+
+    i = 0  # Initial index of first subarry
+    j = 0  # Initial index of second subarray
+    k = low  # Initial index of merged subarray
+
+    while i < n1 and j < n2:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    # Copy the rest of the elements
+    while i<n1:
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j<n2:
+        arr[k] = right[j]
+        j += 1
+        k += 1
 
 
 def sort(arr, low, high):
@@ -28,6 +61,7 @@ if __name__ == '__main__':
     arr = [7, 5, 4, 3, 1, 6, 9, 2, 8]
     length = 9
 
-    sort(arr, 0, length)
+    print(arr)
+    sort(arr, 0, length-1)
     print(arr)
 
