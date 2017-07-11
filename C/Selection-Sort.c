de57b5193105f3b66db1036d6a751b77c2bb943e
sTux <esyray15@gmail.com>
2017-07-11 11:00:58 +0530

    Example of Selection Sort in C

---
+++ C/Selection-Sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+void print(int *arr, int len) {
+    printf("[");
+    for(int i=0; i<len; ++i)
+        printf("%d ", arr[i]);
+    printf("]\n");
+}
+
+
+void sort(int *arr, int len) {
+    int min = 0;
+
+    for(int i=0; i<len-1; ++i) {
+        min = i;
+        for(int j=i+1; j<=len; ++j) {
+            if(arr[j] < arr[min])
+                min = j;
+        }
+
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+
+int main() {
+    int arr[9] = {9, 7, 1, 3, 2, 8, 4, 6, 5};
+    int len = 9;
+
+    print(arr, len);
+    sort(arr, len-1);
+    print(arr, len);
+
+    return 0;
+}
