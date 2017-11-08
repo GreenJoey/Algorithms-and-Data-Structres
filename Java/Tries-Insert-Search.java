490a93a64e1c6d85a943eabb07c9448cfa683204
GreenJoey <esyray15@gmail.com>
2017-11-09 00:12:25 +0530

    Tries example

---
+++ Java/Tries-Insert-Search.java
@@ -0,0 +1,84 @@
+class Trie {
+    static final int ALPHABET_SIZE = 26;
+
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+        boolean is_end;
+
+        TrieNode() {
+            this.is_end = false;
+            for(int i=0; i<ALPHABET_SIZE; i++)
+                this.children[i] = null;
+        }
+    };
+
+    static TrieNode root;
+
+    static void insert(String key) {
+        int length = key.length();
+        int index;
+
+        TrieNode pCrawl = root;
+
+        for(int level = 0; level < length; level++) {
+            index = key.charAt(level) - 'a';
+
+            if(pCrawl.children[index] == null)
+                pCrawl.children[index] = new TrieNode();
+
+            pCrawl = pCrawl.children[index];
+        }
+
+        pCrawl.is_end = true;
+    }
+
+    static boolean search(String key) {
+        int length = key.length();
+        int index;
+
+        TrieNode pCrawl = root;
+
+        for(int level=0; level<length; level++) {
+            index = key.charAt(level) - 'a';
+
+            if(pCrawl.children[index] == null)
+                return false;
+
+            pCrawl = pCrawl.children[index];
+        }
+
+        return (pCrawl != null && pCrawl.is_end);
+    }
+
+    public static void main(String args[]) {
+        String keys[] = {"the", "a", "there", "answer", "any",
+                         "by", "bye", "their"};
+        String output[] = {"Not present in trie", "Present in trie"};
+      
+      
+        root = new TrieNode();
+      
+        // Construct trie
+        int i;
+        for (i = 0; i < keys.length ; i++)
+            insert(keys[i]);
+      
+        // Search for different keys
+        if(search("the") == true)
+            System.out.println("the --- " + output[1]);
+        else System.out.println("the --- " + output[0]);
+         
+        if(search("these") == true)
+            System.out.println("these --- " + output[1]);
+        else System.out.println("these --- " + output[0]);
+         
+        if(search("their") == true)
+            System.out.println("their --- " + output[1]);
+        else System.out.println("their --- " + output[0]);
+         
+        if(search("thaw") == true)
+            System.out.println("thaw --- " + output[1]);
+        else System.out.println("thaw --- " + output[0]);
+        
+    }
+}
